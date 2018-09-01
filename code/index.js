ca3334846ae2a12207486f2e2a9b13016e6b09d3
spaceTab <93plaistj@protonmail.ch>
2018-08-31 20:13:04 -0400

    some optimization & comments

--- code/index.js
+++ code/index.js
@@ -11,16 +11,16 @@ const wordBank = [
     'slackware',
     'redstar'
 ]
-
+//
 let findIndx = 0;
-let chosen   = '';
-let word     = '';
-let count    = 0; //user guesses
+//let chosen   = '';  
+let word     = '';  
+let count    = 0;   //user guesses
 
-
+// Creates an Ascii art greeting @ initiale running
 const LOGO = () => {
     let log = console.log;
-    figlet('Guess Dat Distro', {
+    figlet('Guess The Distro', {
         horizontalLayout: 'default',
         verticalLayout: 'default'
     }, function (err, data) {
@@ -33,25 +33,26 @@ const LOGO = () => {
     });
 }
 
+//function to choose word @ beginning of game
 const BEGIN = () => {
-
-    findIndx = Math.floor(Math.random() * wordBank.length);
-    chosen = wordBank[findIndx];
+    findIndx = Math.floor(Math.random() * wordBank.length); //choses index of an array
+    chosen = wordBank[findIndx]; //takes random index and then choses the word @ that index
     word = new Word(chosen);
-    word.init_word();
+    word.init_word();   //initialize word in word.js
 
     if (findIndx > -1) {
         wordBank.slice(findIndx, 1);
     }
     console.log('You have 10 guessses');
 
-    USR_PROMPT();
+    LETTER_PROMPT();
 }
 
-const USR_PROMPT = () => {
-    if (count < 10) {
+//function that allows user to enter a letter.
+const LETTER_PROMPT = () => {
+    if (count < 10) {       //makes sure the user has correct amount of gueses
         
-        word.show();
+        word.show();    //shows the '_' placeholder of the chosen word
 
         inquirer.prompt([
             {
@@ -75,45 +76,54 @@ const USR_PROMPT = () => {
     }
 }
 
+//function checks if the users input has same letter of chosen word
 const CHECK_ANSWER = (data) => {
+    //ensures input is 1 character -> Regex function to make sure of input is w/ in alphabet
+    if ((data.letter.length === 1) && /^[a-zA-Z]+$/.test(data.letter)) {
+      //  let checker = data.letter.toUpperCase();
+        let letterCheck     = word.solved(data.letter);
+        //word.guess(checker);
 
-    if ((data.letter.length === 1)) {
-        let checker = data.letter.toUpperCase();
-        let tmp = word.solved(data.letter);
-        word.guess(checker);
-
-        if (tmp === word.solved(data.letter)) {
+        if (letterCheck === word.solved(data.letter)) { //compares input to letters in word if false -1 guess
             count++;
             console.log(`Guesses Remaining:  ${10 - count}`);
         } 
         
-        if (tmp === word.guess(data.letter)){
-            USR_PROMPT();
+        if (letterCheck === word.guess(data.letter)){ 
+            LETTER_PROMPT();
         } else {
             CHECK_RIGHT();
         }
     } else {
-        console.log('One Letter @ a time \n');
+        console.log('One Letter @ a time please \n');
         BEGIN();
     }
 }
 
+//function to check if all inputed letters are in the the chosen word
 const CHECK_RIGHT = () => {
     // console.log('correct \n');
     if (word.solved()) {
-        console.log('Correct Guess');
+        figlet('CORRECT!', {
+            horizontalLayout: 'default',
+            verticalLayout: 'default'
+        }, function (err, data) {
+            if (err) return console.log(`ERROR: ${err}}`);
 
-        word   = '';
-        chosen = '';
-        select = 0;
-        count  = 0;
+            word  = '';     //after figlet resets all the variables to ' 0 ' for next round
+            chosen = '';
+            select = 0;
+            count  = 0;  
 
-        NEW_ROUND();
+            console.log(data);
+            NEW_ROUND(); 
+        });        
     } else {
-        USR_PROMPT();
+        LETTER_PROMPT(); //calls this when the word is not solved.
     }
 }
 
+//function to prompt user if they'd like to play again, then responds accordingly
 const NEW_ROUND = () => {
     inquirer.prompt([
         {
@@ -128,12 +138,10 @@ const NEW_ROUND = () => {
         } else {
             figlet('Goodbye!', {
                 horizontalLayout: 'default',
-                verticalLayout: 'default'
+                verticalLayout:   'default'
             }, function (err, data) {
-                if (err) {
-                    console.log(`ERROR: ${err}`);
-                    return;
-                }
+                if (err) return console.log(`ERROR: ${err}`);
+                
                 console.log(`${data}`);
             });
         }
